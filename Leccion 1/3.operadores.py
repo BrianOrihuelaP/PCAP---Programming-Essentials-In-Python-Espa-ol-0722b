c6309f636422e67905eaf8a98ab800702f1592eb
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-07-25 21:44:42 -0500

    Agregando hasta 2.1.3.6

---
+++ Leccion 1/3.operadores.py
@@ -0,0 +1,19 @@
+# OPERADORES-------------------------------------------------------------------------
+# Un operador es un símbolo del lenguaje de programación, el cual es capaz de realizar operaciones con los valores.
+
+# Por ejemplo, como en la aritmética, el signo de + (mas) es un operador el cual es capaz de sumar dos numeros, dando el resultado de la suma.
+# Sin embargo, no todos los operadores de Python son tan simples como el signo de mas, veamos algunos de los operadores disponibles en Python, 
+# las reglas que se deben seguir para emplearlos, y como interpretar las reglas que realizan.
+
+# Se comenzará con los operadores que están asociados con las operaciones aritméticas más conocidas:
+# +, -, *, /, //, %, **
+
+print("Suma: ",2+2)
+print("Resta: ",10-6)
+print("Multiplicación: ",3*4)
+print("Divición: ",23/10)
+print("División entera: ",100//20)
+print("Modúlo: ",10%3)
+print("Exponencial ",2**4)
+
+#2.1.3.6 ....
