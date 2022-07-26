ba0218e75e28734352456be3f575cbc01d69cc16
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-07-25 21:07:40 -0500

    Subiendo curso cisco python essentials

---
+++ Leccion 1/literales.py
@@ -0,0 +1,13 @@
+#Literales en python-----------
+print("2") #cadena
+print(2) # literal entera
+print(1_111_111) #literal con _ para una mejor comprensenion del numero
+#utilizando el sistema octal:
+print(0o123)
+#python hace la conversion automaticamente al igual que en hexadecimal
+print(0x123)
+#numero flotante
+print("Soy un flotante: ", 4.23)
+print("Exponencial e: ",3e10)
+print("Soy la constante de planck (h): ",6.62607e-34)
+print("Python me muestra numeros pequeños en notacion cientifica: ",0.0000000000000000000001)
